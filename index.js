432959e75890951e7045af9ba125efabcd10124d
Reuben T Allik <reubenallik@gmail.com>
2021-03-07 01:01:40 -0500

    random colors

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
+function getRandomColor(){
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
 function createShape(elements) {
     const canvas = document.getElementById("shape");
     const ctx = canvas.getContext("2d");
-    canvas.width = canvas.clientWidth;
-    console.log(canvas.width)
-    canvas.height = canvas.clientHeight;
-    console.log(canvas.height)
+    // canvas.width = canvas.clientWidth;
+    canvas.width = 1916;
+    // console.log(canvas.width)
+    // canvas.height = canvas.clientHeight;
+    canvas.height = 1112;
+    // console.log(canvas.height)
     
-    for (i of elements) {
-        console.log(i.value)
+    for (let x of elements) {
+        console.log(x.value)
     }
 
     // access form input
@@ -23,6 +29,10 @@ function createShape(elements) {
     const shape_h = elements.rectangle_h.value;
     const shape_w = elements.rectangle_w.value;
 
+    const is_random = elements.random.value == 'yes' ? true : false;
+    let shape_color = is_random ? getRandomColor() : elements.color_hex.value;
+
+    
 
 
     let center_h = canvas.height / 2;
@@ -33,17 +43,27 @@ function createShape(elements) {
     let topcorner_x = center_w - (shape_w/2);
     let topcorner_y = center_h - (shape_h/2);
 
-    ctx.fillStyle = 'green';
+    ctx.fillStyle = shape_color;
     ctx.fillRect(topcorner_x, topcorner_y, shape_w, shape_h);
+
+    for (let i; i > shape_count; i++) {
+
+        ctx.fillStyle = shape_color;
+        ctx.fillRect(topcorner_x, topcorner_y, shape_w, shape_h);
+    }
+        
+
+
 }
 
 function onSubmit(event) {
-    event.preventDefault()
+    
     console.log(event.target.elements)
     let forminput = event.target.elements;
     let typecheck = forminput.type.value;
     
     createShape(forminput)
+    event.preventDefault()
 }
 
 
