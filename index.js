ce8e7eb54091e85446287730b05cda04f56e58e4
Reuben T Allik <reubenallik@gmail.com>
2021-03-08 22:25:20 -0500

    added circle

--- index.js
+++ index.js
@@ -5,8 +5,18 @@ function canvasInit() {
     canvas.width = 1916;
     canvas.height = 1112;
 
-    return canvas, ctx;
+    return {canvas, ctx};
 } 
+
+function getRandomColor(){
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function getColor(random) {
+    const is_random = random == 'yes' ? true : false;
+    let color = is_random ? getRandomColor() : inputs_rect.color_hex.value;
+    return color;
+}
 
 function getCanvasCenter(height, width) {
     let y = height / 2;
@@ -16,32 +26,25 @@ function getCanvasCenter(height, width) {
 
 function createRectangle(count_rect = 1, inputs_rect) {
     console.log("inputs_rect",inputs_rect)
-    const canvas_rect = document.getElementById("shape");
-    const ctx_rect = canvas_rect.getContext("2d");
-
-    canvas_rect.width = 1916;
-    canvas_rect.height = 1112;
+    const canvas_init_r = canvasInit();
+    const canvas_rect = canvas_init_r.canvas;
+    const ctx_rect = canvas_init_r.ctx;
 
     const rect_h = inputs_rect.rectangle_h.value;
     const rect_w = inputs_rect.rectangle_w.value;
 
-    let rect_center = getCanvasCenter(rect_h, rect_w);
-    console.log(rect_center)
+    let rect_center = getCanvasCenter(canvas_rect.height, canvas_rect.width);
 
     let rect_center_h = rect_center.y;
     let rect_center_w = rect_center.x;
-    console.log(rect_center_h, rect_center_w)
-
     
     let topcorner_y = rect_center_h - (rect_h/2);
     let topcorner_x = rect_center_w - (rect_w/2);
 
-    console.log(topcorner_x, topcorner_y)
+    let rect_color_random = inputs_rect.random.value;
+    let rect_color = getColor(rect_color_random);
 
-    const is_random = inputs_rect.random.value == 'yes' ? true : false;
-    let shape_color = is_random ? getRandomColor() : inputs_rect.color_hex.value;
-
-    ctx_rect.fillStyle = shape_color;
+    ctx_rect.fillStyle = rect_color;
     ctx_rect.fillRect(topcorner_x, topcorner_y, rect_w, rect_h);
 }
 
@@ -57,30 +60,32 @@ function createTriangle(count_tri = 1, inputs_tri) {
 
 function createCircle(count_cir = 1, inputs_cir) {
     console.log("inputs_cir",inputs_cir)
+    const canvas_init_c = canvasInit();
+    const canvas_cir = canvas_init_c.canvas;
+    const ctx_cir = canvas_init_c.ctx;
+
+    let cir_center = getCanvasCenter(canvas_cir.height, canvas_cir.width);
+    let cir_center_h = cir_center.y;
+    let cir_center_w = cir_center.x;
+
+    let radius = inputs_cir.circle_r.value;
+
+    let cir_color_random = inputs_cir.random.value;
+    let cir_color = getColor(cir_color_random);
+
+    ctx_cir.fillStyle = cir_color;
+    ctx_cir.beginPath();
+    ctx_cir.arc(cir_center_w, cir_center_h, radius, 0, 2 * Math.PI, false)
+    ctx_cir.fill()
 }
 
-function getRandomColor(){
-    return "#" + Math.floor(Math.random()*16777215).toString(16);
-}
 
 function whichShape(elements) {
-    
-    // console.log(canvas.height)
-    
-    // for (let x of elements) {
-    //     console.log(x.value)
-    // }
-
-    // access form input
-    // const shape_count = elements.quantity.value;
     const shape_count = 1;
     console.log(shape_count);
 
     const shape_type = elements.type.value;
     console.log(shape_type);
-
-    // const shape_radius = elements.circle.value;
-    // console.log(shape_radius)
 
     switch (shape_type.toLowerCase()) {
         case "rectangle":
@@ -93,21 +98,6 @@ function whichShape(elements) {
             createCircle(shape_count, elements);
             break;
     }
-
-
-
-
-
-    
-
-    // for (let i; i < shape_count; i++) {
-
-    //     ctx.fillStyle = shape_color;
-    //     ctx.fillRect(topcorner_x, topcorner_y + (shape_h*1.5), shape_w, shape_h);
-    // }
-        
-
-
 }
 
 function onSubmit(event) {
